7526545dfc29cc6ba556a321ac2a9adab8877566
Nandhukriss <103727372+Nandhukriss@users.noreply.github.com>
2023-12-01 03:35:21 +0530

    updates in ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,7 +43,7 @@ return <div className="App flex flex-col justify-center items-center">
   <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20"></div>
   <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center"></div>
   <div className="mx-auto max-w-2xl lg:max-w-4xl">
-  <h1 className="text-5xl text-gray-800">GitHub Profile Scrapper</h1>
+  <h1 className="text-5xl text-gray-800">GitHub Profile Scraper</h1>
     <figure className="mt-10">
       <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 sm:text-2xl sm:leading-9">
         <p>“This is a Simple Github Profile Data Scrapper using Flask And React”</p>
